608b288c9ad4c29f952be73f5aa790278cd9b1e0
dataPuzzler <dataPuzzler@gmail.com>
2021-08-16 12:51:35 +0200

    Change Python Requriement to >= 3.7

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.8, <4',
+    python_requires='>=3.7, <4',
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
